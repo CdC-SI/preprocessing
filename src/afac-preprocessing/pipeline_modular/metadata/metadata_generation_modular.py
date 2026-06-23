c6d2c1fce6d54d77abc6e8364b9140dab3b99a45
MatthiasJutzi <matthias.jutzi@bluewin.ch>
2026-06-23 16:47:37 +0200

    uuid a controler voir commentaire

--- src/afac-preprocessing/pipeline_modular/metadata/metadata_generation_modular.py
+++ src/afac-preprocessing/pipeline_modular/metadata/metadata_generation_modular.py
@@ -388,7 +388,7 @@ def build_metadata(
     page_count = get_page_count(doc_json)
 
     return {
-        "id": str(uuid.uuid4()),
+        "id": str(uuid.uuid4()), # peut etre mettre uuid5 pour sha-1
         "user_uuid": "",
         "source": hierarchy["source"],
         "title": doc_name_extension,
